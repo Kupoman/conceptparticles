9a451b82cc7c26a9f2fb2fc5b6cca24b4bee300d
Daniel Stokes <kupomail@gmail.com>
2013-07-16 19:09:49 -0700

    Adding the start of texture support. For now the texturing makes assumptions about subuv information, and always grabs subuv 0.

--- particles/fragment.glsl
+++ particles/fragment.glsl
@@ -1,4 +1,22 @@
+#version 120
+#extension GL_EXT_gpu_shader4 : enable
+
+uniform sampler2D texture;
+
 void main()
 {
-	gl_FragColor = vec4(0.5, 1.0, 0.0, 0.5);
+	int subuv_divs = 2;
+	int subuv_count = 3;
+	
+	int subuv = 0;
+	float offset = 1.0/subuv_divs;
+	vec2 coord = gl_PointCoord;
+	coord.y = 1 - coord.y;
+	coord *= offset;
+	coord.x += offset * (subuv%subuv_divs);
+	coord.y += offset * (subuv/subuv_divs);
+	
+	vec4 color = texture2D(texture, coord);
+	
+	gl_FragColor = color;
 }
