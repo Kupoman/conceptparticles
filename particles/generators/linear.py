0b94a91312dd04cba845090e2a17fa05ed8b6a03
Daniel Stokes <kupomail@gmail.com>
2013-07-21 23:49:44 -0700

    Adding an out_type attribute to the generators, cleaning up some of the names in the type maps, and putting the type maps into OrderedDicts.

--- particles/generators/linear.py
+++ particles/generators/linear.py
@@ -1,16 +1,21 @@
+from collections import OrderedDict
+
+
 from .common import Generator
 
-class Linear(Generator):	
-	type_map =	{
-					"m" : "FLOAT",
-					"b" : "FLOAT",
-				}
+class Linear(Generator):
+	out_type = "SCALAR"
+
+	type_map =	OrderedDict((
+					("slope", "SCALAR"),
+					("intercept", "SCALAR"),
+				))
 
 	__slots__ = type_map.keys()
 
 	def __init__(self, m=0.05, b=0.0):
-		self.m = m
-		self.b = b
-		
+		self.slope = m
+		self.intercept = b
+
 	def get_value(self, x):
-		return self.m * x + self.b
+		return self.slope * x + self.intercept
--- particles/generators/mix.py
+++ particles/generators/mix.py
@@ -1,31 +1,33 @@
 from .common import Generator
 
 class Mix3(Generator):
-	type_map =	{
-					"gen1" : "GENERATOR",
-					"gen2" : "GENERATOR",
-					"gen3" : "GENERATOR",
-				}
-				
+	out_type = "VECTOR"
+
+	type_map =	OrderedDict((
+					("x_generator", "GENERATOR_SCALAR"),
+					("y_generator", "GENERATOR_SCALAR"),
+					("z_generator", "GENERATOR_SCALAR"),
+				))
+
 	__slots__ = type_map.keys()
-	
+
 	def __init__(self, factory, n1="", d1={}, n2="", d2={}, n3="", d3={}):
-		self.gen1 = self.gen2 = self.gen3 = None
-		
+		self.x_generator = self.y_generator = self.z_generator = None
+
 		if n1:
-			self.gen1 = factory(n1, d1)
+			self.x_generator = factory(n1, d1)
 		if n2:
-			self.gen2 = factory(n2, d2)
+			self.y_generator = factory(n2, d2)
 		if n3:
-			self.gen3 = factory(n3, d3)
-			
+			self.z_generator = factory(n3, d3)
+
 	def get_value(self, x):
 		value = [0, 0, 0]
-		if self.gen1:
-			value[0] = self.gen1.get_value(x)
-		if self.gen2:
-			value[1] = self.gen2.get_value(x)
-		if self.gen3:
-			value[2] = self.gen3.get_value(x)
-			
+		if self.x_generator:
+			value[0] = self.x_generator.get_value(x)
+		if self.y_generator:
+			value[1] = self.y_generator.get_value(x)
+		if self.z_generator:
+			value[2] = self.z_generator.get_value(x)
+
 		return value
