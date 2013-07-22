3476fc1fcd57a63c6da0f5aae58a30ac5141d609
Daniel Stokes <kupomail@gmail.com>
2013-07-21 19:16:40 -0700

    Making the fragment shader compatible with GLSL version 1.20.

--- particles/fragment.glsl
+++ particles/fragment.glsl
@@ -1,26 +1,23 @@
-#version 150
-#extension GL_EXT_gpu_shader4 : enable
+#version 120
 
 uniform sampler2D texture;
 
 flat in vec4 color;
 
-out vec4 frag_color;
-
 void main()
 {
 	int subuv_divs = 2;
 	int subuv_count = 3;
-	
+
 	int subuv = 0;
 	float offset = 1.0/subuv_divs;
 	vec2 coord = gl_PointCoord;
 	coord.y = 1 - coord.y;
 	coord *= offset;
-	coord.x += offset * (subuv%subuv_divs);
+	coord.x += offset * (subuv - subuv_divs*(subuv/subuv_divs));
 	coord.y += offset * (subuv/subuv_divs);
-	
+
 	vec4 text = texture2D(texture, coord);
-	
-	frag_color = text * color;
+
+	gl_FragColor = text * color;
 }
